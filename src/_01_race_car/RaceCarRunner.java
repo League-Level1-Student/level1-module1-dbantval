95fbb74c95d397dd80ac7f186d5de6e67e97a5da
dbant <dbant@LAPTOP-R1FQPLFT>
2021-05-18 18:26:33 -0700

    Yes

--- src/_01_race_car/RaceCarRunner.java
+++ src/_01_race_car/RaceCarRunner.java
@@ -4,6 +4,8 @@ package _01_race_car;
  *    Level 1
  */
 
+private int positionInRace;
+public boolean damaged = false;
 
 
 public class RaceCarRunner {
@@ -11,7 +13,12 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+		public RaceCar(String brand, int positionInRace) {
+			System.out.println("Your " +brand +" race car has been created.");
+			if (positionInRace <= 0)
+				throw new IllegalArgumentException("The car's position cannot be negative.");
+			else if (positionInRace > 15) 
+				throw new IllegalArgumentException("There are only 15 cars in the race.");
 		// 2. Print the RaceCar's position in the race
 
 		// 3. Crash the RaceCar
